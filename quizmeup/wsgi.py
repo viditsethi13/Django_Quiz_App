0f14f4485657170224a965fb3c08a817903935cd
vidit sethi <viditsethi13@gmail.com>
2022-03-25 15:47:38 -0400

    Solution

--- quizmeup/wsgi.py
+++ quizmeup/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for kbc project.
+WSGI config for quizmeup project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kbc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizmeup.settings')
 
 application = get_wsgi_application()
